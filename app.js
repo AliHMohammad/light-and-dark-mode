55fa2324129e8245af779e85960b449f2cb4ec87
AliHMohammad <alihmohammad@hotmail.com>
2023-03-15 11:44:14 +0100

    2312

--- app.js
+++ app.js
@@ -5,9 +5,59 @@ window.addEventListener("load", initApp);
 function initApp() {
     //event listener for select on HTML
     document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+    detectUserPreference();
+    
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
     console.log("New color mode selected");
+    console.log(this);
+    console.log(this.value);
+
+    let selectedColor = this.value;
+    changeMode(selectedColor);
+    saveUserColorMode(selectedColor)
 }
+
+
+function changeMode(mode) {
+    console.log("SALAM!");
+    resetColorMode();
+
+    if (mode == "dark") {
+        document.querySelector("body").classList.add("dark-mode")
+    } else if (mode == "light") {
+        document.querySelector("body").className = "";
+    } else if (mode == "green") {
+        document.querySelector("body").classList.add("green-mode")
+    } else if (mode == "yellow") {
+        document.querySelector("body").classList.add("yellow-mode")
+    }
+
+}
+
+function resetColorMode() {
+    document.querySelector("body").className = "";
+}
+
+
+function saveUserColorMode(mode){
+    localStorage.setItem("userColorMode", mode);
+}
+
+function readUserColorMode() {
+    const userColorMode = localStorage.getItem("userColorMode");
+    return userColorMode;
+}
+
+function detectUserPreference() {
+    const modeFromLocalStorage = readUserColorMode();
+    console.log(modeFromLocalStorage);
+
+    if (modeFromLocalStorage !== null) {
+        changeMode(modeFromLocalStorage);
+        document.querySelector("#select-color-mode").value = modeFromLocalStorage; 
+    }
+}
+
